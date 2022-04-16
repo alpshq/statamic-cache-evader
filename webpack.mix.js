6ee887ccbdc3eef3cacb7d9d8b5f2d46b9138085
Jakub Jo <jakub@jakub.io>
2022-04-16 18:50:07 +0200

    Add source maps

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,5 @@ const mix = require('laravel-mix');
 mix
   .js('resources/js/cache-evader.js', 'dist/js')
   .copyDirectory('dist', '../../public/vendor/statamic-cache-evader')
+  .sourceMaps()
   .disableNotifications();
